f93fcd1fd62fb37daf042d3b5ec1d1851314406b
Samir Damle <damlemail@gmail.com>
2020-07-26 22:19:41 -0400

    Minor update to main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,23 +3,25 @@ module.exports = {
     stories: ['../src/components/**/*.stories.[tj]sx'],
     addons: ['@storybook/addon-knobs/register'],
     webpackFinal: async (config, { configType }) => {
-        config.module.rules.push({
-            test: /\.scss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader'],
-            include: path.resolve(__dirname, '../src'),
-        })
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            use: [
-                {
-                    loader: require.resolve('ts-loader'),
-                },
-                // Optional
-                {
-                    loader: require.resolve('react-docgen-typescript-loader'),
-                },
-            ],
-        })
+        config.module.rules.push(
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+                include: path.resolve(__dirname, '../src'),
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
+                    {
+                        loader: require.resolve('ts-loader'),
+                    },
+                    // Optional
+                    {
+                        loader: require.resolve('react-docgen-typescript-loader'),
+                    },
+                ],
+            },
+        )
         config.resolve.extensions.push('.ts', '.tsx')
         return config
     },
